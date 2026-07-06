2fe1f41226486d1944d6d9dc081e62bb682f581f
Kareena Patidar <kareenapatidar329@gmail.com>
2026-07-07 01:23:40 +0530

    Add files via upload

---
+++ 520A (Panagram)/main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class main{
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String str = sc.nextLine();
+        str = str.toLowerCase();
+
+        HashSet<Character> set = new HashSet<>();
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch>= 'a' && ch<= 'z'){
+                set.add(ch);
+            }
+            
+        }
+        if(set.size() == 26){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
